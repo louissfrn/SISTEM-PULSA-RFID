112bfe4632c9501a0b1d5812469e03f6555b8989
Louis <2003louisfrt@gmail.com>
2025-12-17 19:22:58 +0700

    fix:db2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,11 @@
-// =======================================
-// Database Configuration
-// File: backend/config/database.js
-// =======================================
-
 const mysql = require('mysql2/promise');
 
-// Create connection pool
 const pool = mysql.createPool({
-  host: 'localhost',             
-  user: 'root',                  
-  password: '',                   
-  database: 'sistem_rfid_lijaya',  
-  port: 3306,                      
+  host: 'interchange.proxy.rlwy.net',
+  user: 'root',
+  password: 'GuNRzsposwpMYpJnUXplXpzTvKkbJbJi',
+  database: 'railway',
+  port: 19399,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -21,21 +15,18 @@ const pool = mysql.createPool({
   reconnect: true
 });
 
-// Test database connection
 const testConnection = async () => {
   try {
     const connection = await pool.getConnection();
-    console.log('Database pool connected successfully to LOCAL MySQL');
+    console.log('Database pool connected successfully');
     connection.release();
   } catch (error) {
     console.error('Database pool connection failed:', error.message);
   }
 };
 
-// Call test connection
 testConnection();
 
-// Export pool untuk digunakan di file lain
 module.exports = { 
   pool,
   testConnection 
